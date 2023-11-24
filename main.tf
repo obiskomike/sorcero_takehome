b05c51036d41521bd0d74b509968fd18089b43d0
Michael Obi <obissmichael@gmail.com>
2023-11-24 17:28:33 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,8 @@
+module "sandbox_environment" {
+  source              = "./modules/sandbox_env"
+  project_name_suffix = "dev1"
+  org_id              = "your-org-id"
+  billing_account     = "your-billing-account-id"
+  region              = "us-central1"
+}
 
