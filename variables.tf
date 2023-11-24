c3ff2edc8a0e656b0fc31d7392987d5c18bd1c11
Michael Obi <obissmichael@gmail.com>
2023-11-24 16:13:14 -0600

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,1 +1,11 @@
+variable "gcp_project" {
+  description = "The GCP project ID"
+  type        = string
+}
+
+variable "gcp_region" {
+  description = "The GCP region"
+  type        = string
+  default     = "us-central1" 
+}
 
